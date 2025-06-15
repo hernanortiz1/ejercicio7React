530fcdba1a8938ea75c64a858c90b9ba23a54d64
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-15 15:05:00 -0300

    agregue bootstrap y react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-  return <></>;
+  return <>
+    <main className="container my-4">
+        <h1 className="text-center">Lista de tareas</h1>
+        <FormularioTarea/>
+      </main>
+      <footer className="bg-dark text-light text-center py-3">&copy; Todos los derechos reservados</footer>
+
+  </>;
 }
 
 export default App;
