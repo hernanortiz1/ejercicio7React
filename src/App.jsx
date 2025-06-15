ae9965dd0688de4ea1e86f4db72a2760e1c42479
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-15 16:39:32 -0300

    baje footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return <>
     <main className="container my-4">
         <h1 className="text-center">Lista de tareas</h1>
-        <FormularioTarea/>
       </main>
       <footer className="bg-dark text-light text-center py-3">&copy; Todos los derechos reservados</footer>
 
