e92cefb7cff38f523bf24c9670b18a633350ba85
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-15 23:29:30 -0300

    agregue imagenes a assets y complete TarjetaEmpleado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import ListaEmpleados from "./components/ListaEmpleados"
 
 function App() {
   return <>
     <main className="container my-4">
-        <h1 className="text-center">Lista de tareas</h1>
+        <h1 className="text-center">Lista de empleados</h1>
+        <ListaEmpleados/>
       </main>
       <footer className="bg-dark text-light text-center py-3">&copy; Todos los derechos reservados</footer>
 
---
+++ src/components/TarjetaEmpleado.jsx
@@ -0,0 +1,32 @@
+import { Card, CardGroup, Form, Button } from "react-bootstrap";
+// import {man, man1, man2, woman1, woman2, woman3} from "../assets"
+import man from "../assets/man.png";
+
+const TarjetaEmpleado = () => {
+  return (
+    <section className="p-3 ">
+      <div className="card mb-3 shadow ">
+        <div className="row g-0 align-items-center">
+          <div className="col-md-2">
+            <Card.Img variant="top" src={man} className="imgEmpleado" />
+          </div>
+          <div className="col-md-10">
+            <Card>
+              <Card.Title className="m-2">Nombre: </Card.Title>
+              <Card.Body className="fondoFormulario">
+                <Card.Text>
+                  CEO
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                Business
+              </Card.Footer>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TarjetaEmpleado;
