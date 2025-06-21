40728da874ebe11014b982b1344e22f04ca19523
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-20 23:21:52 -0300

    agregue color de fondo a lista y badge a departamento

--- src/components/TarjetaEmpleado.jsx
+++ src/components/TarjetaEmpleado.jsx
@@ -1,11 +1,11 @@
 import { Card } from "react-bootstrap";
-// import {man, man1, man2, woman1, woman2, woman3} from "../assets"
+import { Badge } from "react-bootstrap";
 
 const TarjetaEmpleado = ({ empleadoProps }) => {
   const { fullName, title, department, pic } = empleadoProps;
 
   return (
-    <section className="p-3 ">
+    <section className="p-3 bg-primary-subtle">
       <div className="card mb-3 shadow border-primary">
         <div className="row g-0 align-items-center">
           <div className="col-md-2 d-flex justify-content-center">
@@ -16,14 +16,14 @@ const TarjetaEmpleado = ({ empleadoProps }) => {
             />
           </div>
 
-          <div className="col-md-10">
-            <Card className="bg-body-secondary">
+          <div className="col-md-10 my-3 ">
+            <Card className="bg-body-secondary ">
               <Card.Title className="m-2">Nombre: {fullName}</Card.Title>
-              <Card.Body className="fondoFormulario">
+              <Card.Body className="p-3 rounded-3 bg-white">
                 <div className="d-flex fw-bold">
                   {title}:
-                  <div className="ms-1 bg-primary-subtle px-2 fw-normal rounded-2">
-                    {department}</div>
+                  <Badge className="ms-1 bg-primary-subtle px-2 fw-normal rounded-2 text-dark">
+                    {department}</Badge>
                 </div>
               </Card.Body>
             </Card>
