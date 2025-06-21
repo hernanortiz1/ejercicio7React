b2dc8fe6a3eb7a58f2986e5bf7859b23902e42b4
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-20 23:02:32 -0300

    corregi link de imagenes

--- src/components/ListaEmpleados.jsx
+++ src/components/ListaEmpleados.jsx
@@ -7,21 +7,21 @@ const ListaEmpleados = () => {
       fullName: "Laya Dueñas",
       title: "CEO",
       department: "Business",
-      pic: "/assets/woman (1).png",
+      pic: "/assets/woman1.png",
     },
     {
       id: 2,
       fullName: "Astryd Vallés",
       title: "CMO",
       department: "Marketing",
-      pic: "/assets/woman.png",
+      pic: "/assets/woman2.png",
     },
     {
       id: 3,
       fullName: "Shantell Meza",
       title: "CFO",
       department: "Business",
-      pic: "/assets/woman_4140078.png",
+      pic: "/assets/woman3.png",
     },
     {
       id: 4,
@@ -35,23 +35,43 @@ const ListaEmpleados = () => {
       fullName: "Ares Jiménez",
       title: "Art Director",
       department: "Marketing",
-      pic: "/assets/man_4140048.png",
+      pic: "/assets/man1.png",
     },
     {
       id: 6,
       fullName: "Juan Pérez",
       title: "Frontend Dev",
       department: "Engineering",
-      pic: "/assets/man_4140079.png",
+      pic: "/assets/man2.png",
     },
-    
+    {
+      id: 7,
+      fullName: "Ellen Balderas",
+      title: "Digital Strategist",
+      department: "Marketing",
+      pic: "/assets/woman1.png",
+    },
+    {
+      id: 8,
+      fullName: "Cynthia Valentín",
+      title: "Backend Dev",
+      department: "Engineering",
+      pic: "/assets/woman2.png",
+    },
+    {
+      id: 9,
+      fullName: "Bernard Jung",
+      title: "DevOps Engineer",
+      department: "Engineering",
+      pic: "/assets/man1.png",
+    },
   ];
 
   return (
     <div className="shadow">
-       {empleados.map((empleado) => (
-      <TarjetaEmpleado key={empleado.id} empleadoProps={empleado} />
-    ))}
+      {empleados.map((empleado) => (
+        <TarjetaEmpleado key={empleado.id} empleadoProps={empleado} />
+      ))}
     </div>
   );
 };
